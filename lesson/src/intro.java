ce4884b7b6503811343f456f5550f57a7fd97510
gaybullayevumid <umidgaybullayev955@gmail.com>
2026-01-05 00:22:36 +0500

    feat: add Java introduction covering JDK, JVM, JRE and basic syntax

---
+++ lesson/src/intro.java
@@ -0,0 +1,11 @@
+//JDK, JVM, JRE
+
+// JVM - Java Virtual Machine
+
+// JVM - kodni ishga tushuradigan mashina, .class faylni o'qib, kompyuterga tushunarli qiladi
+
+// Java kodi togridan togri kampyuterda ishlamaydi JVM orqali ishlaydi
+
+// JRE - Java Runtime Environment
+
+// JRE - JVM, Javaning tayyor kutubxonalari
